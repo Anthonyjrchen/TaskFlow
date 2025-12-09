bb4ead51d3874d964fab7c3125c82f33c9486a39
Anthonyjrchen <anthonyjrchen@gmail.com>
2025-12-08 16:18:40 -0800

    skeleton login ui and routes to /tasks upon successful login.

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { supabase, setAccessToken } from "../services/supabase";
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(""); // Get rid of previous error
+    setLoading(true);
+
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if (error) throw error;
+
+      if (data.session) {
+        console.log("Session token: " + data.session.access_token);
+        setAccessToken(data.session.access_token);
+        navigate("/tasks");
+      }
+    } catch (error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Loading..." : "Submit"}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
